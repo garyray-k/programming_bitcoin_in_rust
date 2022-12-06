ded1242f3e5e53345f4ea2fdd985dddb406c5229
Gary Krause <garyraykrause@outlook.com>
2022-12-05 20:23:01 -0500

    elliptic curve point
    Co-authored-by: pleblira <pleblira@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod field_element;
+pub mod point;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
---
+++ src/point.rs
@@ -0,0 +1,99 @@
+use std::{f64::INFINITY, ops::Add};
+
+#[derive(Clone, Copy)]
+struct Point {
+    x: f64,
+    y: f64,
+    a: i8,
+    b: i8
+}
+
+impl Point {
+    pub fn new(x: f64, y: f64, a: i8, b: i8) -> Self {
+        if (x == INFINITY) || (y == INFINITY) {
+            return Self {
+                x,
+                y,
+                a,
+                b
+            }
+        }
+        if (y.powf(2.0)) != x.powf(3.0) + a as f64 * x + b as f64 {
+            panic!("{}, {} is not on the curve.", x, y);
+        }
+        Self {
+            x,
+            y,
+            a,
+            b
+        }
+    }
+}
+
+impl Add for Point {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        if (other.a != self.a) || (other.b != self.b) {
+            panic!("{}, {} is not on the curve for this Point.", other.x, other.y);
+        }
+
+        // 1. Where the points are in a vertical line or using the identity point
+        if self.x == INFINITY {
+            return other;
+        }
+        if other.x == INFINITY {
+            return self;
+        }
+        if (self.y + other.y == 0.0) && (self.x == other.x) {
+            return Point::new(INFINITY, INFINITY, self.a, self.b);
+        }
+
+        self
+        // 2. Where the points are not in a vertical line, but are different
+        // 3. Where the two points are the same
+
+    }
+}
+
+
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        return self.a == other.a && self.b == other.b && self.x == other.x && self.y == other.y;
+    }
+}
+
+impl Eq for Point {}
+
+#[cfg(test)]
+mod point_tests {
+
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn bad_point() {
+        let b = Point::new(-1.0, -2.0, 5, 7);
+    }
+
+    #[test]
+    fn eq_works() {
+        let a = Point::new(-1.0, -1.0, 5, 7);
+        let b = Point::new(-1.0, -1.0, 5, 7);
+        let c = Point::new(18.0, 77.0, 5, 7);
+        
+        assert!(a == b);
+        assert!(a != c);
+    }
+
+    #[test]
+    fn add_test() {
+        let p1 = Point::new(-1.0, -1.0, 5, 7);
+        let p2 = Point::new(-1.0, 1.0, 5, 7);
+        let identity_point = Point::new(INFINITY, INFINITY, 5, 7);
+        
+        assert!(p1 + identity_point == p1);
+        assert!(p2 + identity_point == p2);
+
+        assert!(p1 + p2 == identity_point);
+    }
+}
